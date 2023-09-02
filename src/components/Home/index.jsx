a7003a9f0bf4e431ef0af237087df60a6c7359de
DuncanCaplan <duncan.caplan@outlook.com>
2023-09-02 18:09:50 +0100

    useState

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 const Home = () => {
-    const handleClick = (name, e) => {
-        console.log(`Hello, ${name}`, e);
+    const [name, setName] = useState("Mario");
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName("Luigi");
+        setAge(26);
     };
     return (
         <div className="home">
             <h2>HomePage</h2>
-            <button onClick={e => handleClick("Bing", e)}>Click me</button>
+            <p>
+                {name} is {age}
+            </p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 };
